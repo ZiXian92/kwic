02d58a9ecd9b04d5c47856bf47031b1f327113ce
Joan Tan <joan.tan.xy@gmail.com>
2016-09-04 19:07:48 +0800

    Adds implementation for RequiredWords component

--- src/main/java/CS3213/RequiredWords.java
+++ src/main/java/CS3213/RequiredWords.java
@@ -18,16 +18,27 @@ public class RequiredWords {
     public RequiredWords(String[] reqWords){
         // This project doesn't seem to use JDK 1.8 so lambda expressions not supported :(
         this.reqWords = new TreeSet<String>();
-        for(String word: reqWords) this.reqWords.add(word);
+        for(String word: reqWords){
+            word = word.trim();
+            if(!word.isEmpty())
+                this.reqWords.add(word.toLowerCase());
+        }
     }
 
     /**
      * Filters out given lines based on its keyword.
-     * A line passes if keyword matches any of its filter words or if there is no fitler word.
+     * A line passes if keyword matches any of its filter words or if there is no filter word.
      * @param lines The lines to be filtered
      * @return Array of strings that pass this filter requirements
      */
     public String[] filter(String[] lines){
-        return lines;
+        TreeSet<String> filteredLines = new TreeSet<String>();
+        if(reqWords.size()<1) return lines;
+        for(String line: lines) {
+            String[] words= line.split("\\s+", 2);
+            if (reqWords.contains(words[0].toLowerCase()))
+                filteredLines.add(line);
+        }
+        return filteredLines.toArray(new String[filteredLines.size()]);
     }
 }
